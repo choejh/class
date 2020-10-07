e088bac979db8ceb45a33e528f13dee08cf92a7e
choejh <choeejihye@gmail.com>
2020-10-07 17:47:12 +0900

    1007

--- javaproject/FirstJava/src/card/Card.java
+++ javaproject/FirstJava/src/card/Card.java
@@ -2,35 +2,26 @@ package card;
 
 public class Card {
 
-	String kind;	//카드의 종류
-	int number;		//카드의 숫자
+	String kind;	// 카드의 종류
+	int number;		// 카드의 숫자
 	
-	Card(String kind, String number){
+	Card(String kind, int number){
 		this.kind = kind;
 		this.number = number;
-	}	
-	Card(){
-		// 다른 생성자를 호출
-		this("spade",1);
 	}
 	
-	//클래스 변수 선언 (Card.width 가능)
-	static int width = 100;	//카드의 폭
-	static int height = 250;	//카드의 높이
+	Card(){
+		// 다른 생성자를 호출
+		this("spade", 1);
+	}
+	
+	
+	
+	// 클래스 변수 선언
+	static int width = 100;	// 카드의 폭
+	static int height = 250;	// 카드의 높이
 	
 	static void play() {
 		System.out.println("카드놀이를 합니다.");
-	
-	
-	Card.play();
-	
-	Card c1  = new Card ();
-	Card C2  = new Card ();
-	
-	// 졸앗음
-	
 	}
-	
-	
-	
 }
