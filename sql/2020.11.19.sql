d5192d0d5868a0c102cae41d5a1b30de40dcb88d
choejh <choeejihye@gmail.com>
2020-11-23 18:07:39 +0900

    1123

--- sql/2020.11.19.sql
+++ sql/2020.11.19.sql
@@ -40,19 +40,46 @@ from member;
 -- 회사이름, 부서이름, 직급
 -- 모임이름, 닉네임
 --=============================
+drop table Contact;
 
+create table Contact (
+ pIdX Number not null primary key,
+ NAME varchar2(12) not null,
+ phonenum varchar2(12) not null,
+ addr varchar2(36) default 'null' not null,
+ email varchar2(36) default 'null' not null,
+ category varchar(12) constraint cg_check check (category in('univ','com','cafe')),
+ uni_major varchar(12),
+ uni_year number,
+ com_companyname varchar(12),
+ com_dname varchar(12),
+ com_job varchar(12),
+ caf_name varchar(12),
+ caf_nickname varchar(12)
+ );
+  
 --2. DEPT 테이블에 데이터를 삽입하는 SQL을 작성하시오. 입력 데이터는 임의로 작성하시오.
+select * from dept;
+insert into dept values(65,'마케팅','서울');
 --3. DEPT 테이블에 위에서 삽입한 데이터의 dname, loc 데이터를 변경하는 SQL을 작성하시오.
 --   입력 데이터는 임의로 작성하시오.
+update dept set dname='전산',loc='인천' where deptno=65;
 --4. DEPT 테이블에 위에서 삽입한 데이터를 deptno 컬럼의 값을 기반으로 삭제하는 SQL을 작성하시오.
+delete from dept where deptno=65; 
 --5. 사용자가 보유한 테이블 목록을 확인하는 SQL문을 작성하시오.
+select table_name from user_tables;
 --6. EMP 테이블의 구조를 확인하는 SQL을 작성하시오.
+desc emp;
 --7. 사용자가 정의한 제약조건들을 확인하는 SQL문을 작성하시오.
-
+select * from user_constraints;
 
 --#2 아래 요구사항에 맞도록 고급 SQL 문을 작성하시오.
 
 --1. EMP 테이블의 ename 컬럼에 인덱스를 생성하는 SQL을 작성하시오. 인덱스의 이름은 emp_index
+create index emp_index on emp(ename);
+
 --2. EMP 테이블과 DEPT 테이블을 조인하는 SQL을 기반으로 view 객체를 생성하는 SQL을 작성하시오.
 --   view 의 이름은 emp_view 로 하시오.
+create or replace view emp_view as select e.ename, e.job, d.deptno, d.dname, d.loc from emp e, dept d where e.deptno=d.deptno;
 --3. EMP 테이블에서 모든 사원의 부서번호를 이름이 'SCOTT'인 사원의 부서번호로 변경하는 SQL을 작성하시오.
+update emp set deptno=(select deptno from emp where ename='SCOTT');
