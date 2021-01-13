24e077a46808a76f198a10dc7313a92fc507a1f5
choejh <71997987+choejh@users.noreply.github.com>
2021-01-13 18:30:58 +0900

    0113

---
+++ spring_project/Spring-OpenProject/src/main/java/com/aia/op/member/dao/MemberDao.java
@@ -0,0 +1,14 @@
+package com.aia.op.member.dao;
+
+import com.aia.op.member.domain.Member;
+import com.aia.op.member.domain.MemberRegRequest;
+
+public interface MemberDao {
+	
+	//public abstract
+	int insertMember(Member member);
+	Member selectLogin(String id, String pw);
+	
+	
+	
+}
